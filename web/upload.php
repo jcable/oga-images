5f0d82cf389b87d2beae6f6e44a2d07b0d2b4a87
Julian Cable <julian.cable@yahoo.com>
2015-11-29 19:48:03 +0000

    doc

--- web/upload.php
+++ web/upload.php
@@ -8,23 +8,33 @@
   
   }else{
 
-    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-    echo "Type: " . $_FILES["file"]["type"] . "<br />";
-    echo "Size: " . number_format($_FILES["file"]["size"] / 1024, 2) . " Kb<br />";
-    foreach($_POST as $k => $v) {
-	    echo "$k: $v<br/>";
-    }
-   
+    $name = $_FILES["file"]["name"];
+
+    echo "Uploaded: $name (" . number_format($_FILES["file"]["size"] / 1024 / 1024, 2) . " MB)<br />";
+
 
     if (file_exists($_FILES["file"]["name"])){
         unlink($_FILES["file"]["name"]);
     }
     
-    if(move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"]["name"])){
-      echo "View image <a href='" . $_FILES["file"]["name"]."' target='_blank'>here</a>";
+    if(move_uploaded_file($_FILES["file"]["tmp_name"],$name)){
+      echo "View image <a href='" . $n."' target='_blank'>here</a>";
     }else{
       echo "The file wasn't uploaded";
     }
+
+    $n = basename($name).".html";
+    $f = fopen($n, "w");
+    fwrite($f, "<html><head><title>".$_POST["caption"]."</title></head><body><ul>\n");
+    foreach($_POST as $k => $v) {
+	    echo "<li>$k: $v</li>";
+    }
+    $a = getimagesize($name);
+    $width = $a[0] / $a[1] * 480;
+    fwrite($f,"</ul><a href='$name'><img src='$name' height='480' width='$width'/></a>");
+    fwrite($f,"</body></html>");
+    fclose($f);
+   
   }
   
 
